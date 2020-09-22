dbf2be4000fed6ee94d973676c568f795da60587
Lucha3sr <2278@holbertonschool.com>
2020-09-22 22:42:18 +0000

    functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,24 @@
 
 /**
  * print_sign - print a sign
- *@n: n
+ * @n: n
  * Return: Always 0.
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		return (0);
 	}
 	if (n < 0)
 	{
-		_putchar("-");
+		_putchar('-');
 		return (-1);
 	}
 }
