444961408549e5977c7ae0add422947f4c3360c0
Lucha3sr <2278@holbertonschool.com>
2020-09-23 00:32:57 +0000

    functions

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print last digit.
+ * @n: n
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int m;
+	m = n % 10;
+	if (m < 0 )
+	{
+		m = m * (-1);
+	}
+	_putchar(m + '0');
+		return (m);
+}
