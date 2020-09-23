d59384fe080a948a4975f2151c1451a4241a3974
Lucha3sr <2278@holbertonschool.com>
2020-09-23 02:33:21 +0000

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,23 +5,23 @@
  *@n: n
  * Return: Always 0.
  */
-void print_to_98(int n);
-
+void print_to_98(int n)
 {
 	int i;
 
 	if (n < 98)
 	{
-		for (a = n; a < 98; a++)
+		for (i = n; i < 98; i++)
 		{
-			printf("%i, ", a);
+			printf("%i, ", i);
 		}
 	}
 	if (n > 98)
 	{
-		for (a = n; a > 98; a--)
+		for (i = n; i > 98; i--)
 		{
-			printf("%i, ", a);
+			printf("%i, ", i);
 		}
 	}
 	printf("98\n");
+}
