901ba502bab89366adfe41b1851bd3df87a64828
Lucha3sr <2278@holbertonschool.com>
2020-09-24 19:10:23 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print a diagonal.
+ *@n: n
+ */
+void print_diagonal(int n)
+{
+	int i, l;
+
+	if (n > 0)
+	{
+		for (l = 1; l <= n; l++)
+		{
+			for (i = 0; i < l; i++)
+			{
+				_putchar (' ');
+			}
+			_putchar ('\\');
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
