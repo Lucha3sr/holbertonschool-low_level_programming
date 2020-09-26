938bf9e0f74f073b3571b90834f66fe4c1d82df1
Lucha3sr <2278@holbertonschool.com>
2020-09-26 00:41:44 +0000

    functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,35 +8,24 @@
 void times_table(void)
 {
 
-	int a, b, c, d, e, f;
+	int a, b, c;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
 		{
-			c = a * b;
-			d = c / 10;
-			e = c % 10;
-		}
-	}
-	if (c > '9')
-	{
-		_putchar(d);
-		_putchar(e);
-	}
-	else
-	{
-		_putchar(c);
-	}
-	if (b != '9')
-	{
-		_putchar(',');
-		_putchar(' ');
-	}
-		f = a * (b + 1);
-		if (c < '9' && f < '9' && b != '9')
-		{
+			_putchar(',');
 			_putchar(' ');
+
+			c = a * b;
+			if (c <= 9)
+				_putchar(' ');
+			else
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
+	}
 }
