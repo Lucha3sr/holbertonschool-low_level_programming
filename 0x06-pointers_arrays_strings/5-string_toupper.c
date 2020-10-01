4f3e4275ef17812427294d618d2580c74a9e3afb
Lucha3sr <2278@holbertonschool.com>
2020-10-01 17:19:05 +0000

    more pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ **string_toupper - check lowercase.
+ *@s: s
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] <= 97 && s[i] >= 122)
+		{
+			s[i] = s[i] - 32;
+			i++;
+		}
+	}
+	return (s[i])
+}
