684080ac291a6b5143c3871399dbc881d39f8ada
Lucha3sr <2278@holbertonschool.com>
2020-10-05 22:50:06 +0000

    even more pointers

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - set the value of string.
+ * @s: pointer to pointer s
+ * @to: pointer to char to
+ * Return: void.
+ */
+void set_string(char **s, char *to)
+{
+        *s = to;
+}
