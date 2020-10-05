176ac9d8f5814e8ff8f9030719f7412538a970e9
Lucha3sr <2278@holbertonschool.com>
2020-10-05 20:40:26 +0000

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ **_strstr  - find first occurrence of string.
+ * @haystack: char haystack
+ * @needle: char needle
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	
