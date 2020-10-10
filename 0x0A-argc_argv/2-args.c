77d361ff266ff5c9540fdf52f1ffa5a310f6872c
Lucha3sr <2278@holbertonschool.com>
2020-10-10 22:34:46 +0000

    C - argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of a program
+ * @argc: argc
+ * @argv: argv
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return(0);
+}
