390ab6ec18824457009e0cacde82d33ddab8c89b
Lucha3sr <2278@holbertonschool.com>
2020-10-10 21:44:24 +0000

    C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: argv
  * Return: Always 0.
  */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n", argv[0])
+	printf("%s\n", argv[0]);
 	return (0);
 }
