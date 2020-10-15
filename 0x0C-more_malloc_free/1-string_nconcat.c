0d43750531dd7e81583cf6d392cde7d04ac8145e
Lucha3sr <2278@holbertonschool.com>
2020-10-15 18:02:44 +0000

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,25 +21,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-		}
-		for (ii = 0; s2[ii] != '\0'; ii++)
-		{
-		}
-		p = malloc(sizeof(char) * (i + ii) + 1);
-			if (p == NULL)
-			{
-				return (NULL);
-			}
-		for (x = 0; s1[x] != '\0'; x++)
-		{
-			p[x] = s1[x];
-		}
-		for (y = 0; s2[y] != '\0' && y < n; y++)
-		{
-			p[x + y] = s2[y];
-			p[x + y] = '\0';
-		}
-		return (p);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+
+	for (ii = 0; s2[ii] != '\0'; ii++)
+	{
+	}
+	p = malloc(sizeof(char) * (i + ii) + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+		p[x] = s1[x];
+	}
+	for (y = 0; s2[y] != '\0' && y < n; y++)
+	{
+		p[x + y] = s2[y];
+	}
+	p[x + y] = '\0';
+	return (p);
 }
