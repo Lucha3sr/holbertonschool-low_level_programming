ce9c57f3119146a0511bb91a0dbe8386098ab28c
Lucha3sr <2278@holbertonschool.com>
2020-10-19 20:39:02 +0000

    C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -7,4 +8,32 @@
  */
 void print_dog(struct dog *d)
 {
-
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+	printf("Name: %s\n", d->name);
+	}
+	if (d->age == '0')
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+	printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
