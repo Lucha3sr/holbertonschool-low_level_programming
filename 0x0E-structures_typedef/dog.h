5a2cda29588407bcbf5e4bfa9b8e0d432f6c8dbb
Lucha3sr <2278@holbertonschool.com>
2020-10-19 19:16:02 +0000

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - define a type struct dog .
+ * @name: name
+ * @owner: owner
+ * @age: age
+ * Return: Always 0.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
