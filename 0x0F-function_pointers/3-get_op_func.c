0f0abc550fb586141b1beb7584e926b53a392d56
Lucha3sr <2278@holbertonschool.com>
2020-10-21 23:38:59 +0000

    pointer to function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-#include "function_pointers.h"
-#include <string.h> 
+#include "3-calc.h"
+#include <string.h>
 
 /**
  * get_op_func - select the correct function
  * @s: operator
- * @int: int
  * Return: 0 if false, something else otherwise.
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 
 	int i = 0;
@@ -29,4 +28,6 @@ int (*get_op_func(char *s))(int, int)
 		}
 		i++;
 	}
+	return (NULL);
 }
+
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "calc.h"
+#include <stdlib.h>
+#include "3-calc.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -11,24 +12,24 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
-	char s;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*' || argv[2] != '/'
-	    || argv[2] != '%')
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+	    && *argv[2] != '/' && *argv[2] != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if (argv[2] == '/' || argv[2] == '%' && argv[3] == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	printf("%d\n", (*get_op_func(argv[2]))(num1, num2));
+	return (0);
 }
