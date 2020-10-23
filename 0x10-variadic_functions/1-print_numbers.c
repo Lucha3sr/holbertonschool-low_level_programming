cacaecdc050556338a4c25d6000b409e1b4c72c3
Lucha3sr <2278@holbertonschool.com>
2020-10-23 21:00:31 +0000

    C - Variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints nbrs
+ * @separator: separator
+ * @n: n
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list a_list;
+
+	va_start(a_list, n);
+
+	if (n == 0)
+	{
+		return;
+	}
+	for (i = 0; i < n - 1; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%d", va_arg(a_list, int));
+		}
+		else
+		{
+			printf("%d%s", va_arg(a_list, int), separator);
+		}
+	}
+	printf("%d\n", va_arg(a_list, int));
+	va_end(a_list);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,51 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings.
+ * @separator: separator
+ * @n: n
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+
+	va_list a_list;
+
+	va_start(a_list, n);
+
+	if (n == 0)
+	{
+		return;
+	}
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+	for (i = 0; i < n - 1; i++)
+	{
+		s = va_arg(a_list, char *);
+
+		if (s == NULL)
+		{
+			printf("(nil)%s", separator);
+		}
+		else
+		{
+			printf("%s%s", s, separator);
+		}
+	}
+	s = va_arg(a_list, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("%s\n", va_arg(a_list, char *));
+	}
+}
