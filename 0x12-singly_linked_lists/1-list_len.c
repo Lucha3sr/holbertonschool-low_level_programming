5eac32abde7ad208653911d62d15dcd8efe9d614
Lucha3sr <2278@holbertonschool.com>
2020-10-29 17:20:53 +0000

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list..
+ * @h: h
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
