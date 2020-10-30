4f21b1aa2506d15353ef77b6795c626ca3314f09
Lucha3sr <2278@holbertonschool.com>
2020-10-30 20:44:51 +0000

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_before(void)__atribute__((constructor))
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
