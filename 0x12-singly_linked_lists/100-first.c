b4595a020c42002c7e98df940031582613bd88b1
Lucha3sr <2278@holbertonschool.com>
2020-10-30 20:55:29 +0000

    C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_before - print attribute.
  *
  * Return: Always 0.
  */
-void print_before(void)__atribute__((constructor))
+void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
