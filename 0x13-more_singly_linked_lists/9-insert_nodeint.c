1908de811a772c2e31a1a175360e2eb430a6ae84
Lucha3sr <2278@holbertonschool.com>
2020-11-04 18:03:09 +0000

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: head
+ * @idx: idx
+ * @n: n
+ * Return: Always 0.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *node, *aux;
+
+	aux = *head;
+	if (!*head)
+	{
+		return (NULL);
+	}
+
+	node = malloc(sizeof(listint_t));
+	if (!node)
+	{
+		return (NULL);
+	}
+	node->n = n;
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+	}
+	else
+	{
+		for (i = 0; i < idx - 1; i++)
+		{
+			if (!aux || !aux->next)
+			{
+				return (NULL);
+			}
+			aux = aux->next;
+		}
+		node->next = aux->next;
+		aux->next = node;
+	}
+	return (node);
+}
