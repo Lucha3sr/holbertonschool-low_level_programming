2ac3dfaa04104f0515ccfa06027920a31c94cb02
Lucha3sr <2278@holbertonschool.com>
2020-11-04 18:15:06 +0000

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,13 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *node, *aux;
-
-	aux = *head;
-	if (!*head)
-	{
-		return (NULL);
-	}
+	listint_t *node, *aux = *head;
 
 	node = malloc(sizeof(listint_t));
 	if (!node)
