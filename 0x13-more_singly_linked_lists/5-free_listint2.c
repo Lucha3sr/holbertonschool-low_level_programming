73a3ac9bc4a28bbb3010e6d60ad8ce88b255e481
Lucha3sr <2278@holbertonschool.com>
2020-11-04 13:17:06 +0000

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		aux = (*head)->next;
-		free(head);
+		aux =(*head)->next;
 		free(*head);
 		*head = aux;
 	}
