c96184dc54475202d3457331bbecb58e48bc6c07
Lucha3sr <2278@holbertonschool.com>
2020-11-09 21:02:23 +0000

    C - File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * create_file - Create a function that creates a file
+ * @filename: integer
+ * @text_content: integer
+ * Return: Holberton project
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
