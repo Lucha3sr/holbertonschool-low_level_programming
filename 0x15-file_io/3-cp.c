71686a55f9687044506aca172d693b7d387b32d5
Lucha3sr <2278@holbertonschool.com>
2020-11-10 17:57:43 +0000

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,17 +16,20 @@ int main(int argc, char **argv)
 	fd1 = open(argv[1], O_RDONLY);
 	leer = read(fd1, buff, 1024);
 	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	escribir = write(fd2, buff, leer);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]), exit(99);
 	while (leer == 1024)
 	{
 		leer = read(fd1, buff, 1024);
 		escribir = write(fd2, buff, leer);
+		if (escribir != leer)
+		{dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]);
+			close(fd2);
+			exit(99);
+		}
 		if (leer == -1)
 		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			close(fd1);
@@ -37,12 +40,12 @@ int main(int argc, char **argv)
 			close(fd2);
 			exit(99);
 		}
-		close1 = close(fd1);
-		close2 = close(fd2);
-		if (close1 == -1)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close1), exit(100);
-		if (close2 == -1)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close2), exit(100);
 	}
-		return (0);
+	close1 = close(fd1);
+	close2 = close(fd2);
+	if (close1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close1), exit(100);
+	if (close2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close2), exit(100);
+	return (0);
 }
