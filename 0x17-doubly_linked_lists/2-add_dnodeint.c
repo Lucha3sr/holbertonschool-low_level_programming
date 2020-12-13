6423b2477d3d302792940f41d8b5ed6586aba115
Lucha3sr <2278@holbertonschool.com>
2020-12-13 04:39:52 +0000

    doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a listint_t list
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
  * @head: head
  * @n: n
  * Return: Always 0.
@@ -17,8 +17,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->next = *head;
 	new_node->prev = NULL;
+	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
