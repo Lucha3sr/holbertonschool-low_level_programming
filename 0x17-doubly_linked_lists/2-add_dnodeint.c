638d3376828be1e3dd28562887d7cb8ff3ab0b25
Lucha3sr <2278@holbertonschool.com>
2020-12-14 02:08:50 +0000

    doubly linked lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,11 +18,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = *head;
-	*head = new_node;
-	if (*head)
+	if (*head == NULL)
 	{
+		*head = new_node;
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *head;
 		(*head)->prev = new_node;
+		*head = new_node;
 	}
 	return (new_node);
 }
