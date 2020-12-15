f253f96ee75caffc5c2361cea4d3c24befa1205c
Lucha3sr <2278@holbertonschool.com>
2020-12-15 01:49:10 +0000

    dll

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,34 +9,31 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *new_node, *aux = *h;
+	dlistint_t *aux = *h, *new_node;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (idx != 1)
+	{
+		aux = aux->next;
+		idx--;
+		if (aux == NULL)
+			return (NULL);
+	}
+
+	if (aux->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-	{
+	if (new_node == NULL)
 		return (NULL);
-	}
+
 	new_node->n = n;
-	if (idx == 0)
-	{
-		new_node->prev = NULL;
-		new_node->next = *h;
-		*h = new_node;
-	}
-	else
-	{
-		for (i = 0; i < idx - 1; i++)
-		{
-			if (!aux || !aux->next)
-			{
-				return (NULL);
-			}
-			aux = aux->next;
-		}
-		new_node->next = aux->next;
-		new_node->prev = aux;
-		aux->next = new_node;
-	}
+	new_node->prev = aux;
+	new_node->next = aux->next;
+	aux->next->prev = new_node;
+	aux->next = new_node;
+
 	return (new_node);
 }
