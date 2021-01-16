7c3316ed542ae08be01fddf89d3b67246efee0e3
Lucha3sr <2278@holbertonschool.com>
2021-01-16 00:20:49 +0000

    C - Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = NULL;
+	hash_node_t *new_node;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
