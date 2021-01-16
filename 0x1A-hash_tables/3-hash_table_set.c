187c03a0d1386b86319a608da4f7fefd47255cbc
Lucha3sr <2278@holbertonschool.com>
2021-01-16 00:28:08 +0000

    C - Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,22 +2,23 @@
 
 /**
  * hash_table_set - adds an element to the hash table
- * @ht: is the hash table you want to add or update the key/value
- * @key: is the key, cannot be an empty string
- * @value: is the value associated with the key
+ * @ht: the hash table you want to add or update the key/value
+ * @key: the key, cannot be an empty string
+ * @value: the value associated with the key
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *tmp, *new_node;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	if (*key == '\0')
 		return (0);
 
+	tmp = strdup(value);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 
@@ -26,9 +27,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	new_node->value = tmp;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
