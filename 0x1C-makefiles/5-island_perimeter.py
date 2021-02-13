f7259c834fcd6ddacbd3d86ee972879dc0b97008
Lucha3sr <2278@holbertonschool.com>
2021-02-13 00:28:33 +0000

    C - Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,9 +11,9 @@ def island_perimeter(grid):
 
     perimeter = 0
     for x in range(len(grid)):
-        for y in range(len(grid[0])):
+        for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                if ((grid[x][y-1] == 0) and (grid[x][y+1] == 0)):
+                if ((grid[x][y-1] == 0) or (grid[x][y+1] == 0)):
                     perimeter += 1
                 if ((grid[x-1][y] == 0) or (grid[x+1][y] == 0)):
                     perimeter += 1
