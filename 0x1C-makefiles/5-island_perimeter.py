af928d913c0dc32b7d6fbaf73632a4563f6da245
Lucha3sr <2278@holbertonschool.com>
2021-02-13 00:07:41 +0000

    C - Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
 """ returns the perimeter of the island described in grid"""
 """ 0 represents a water zone, 1 represents a land zone"""
 
+
 def island_perimeter(grid):
 
     """Grid is completely surrounded by water, and there is one island"""
@@ -13,7 +14,7 @@ def island_perimeter(grid):
         for y in range(len(grid[0])):
             if grid[x][y] == 1:
                 if ((grid[x][y-1] == 0) and (grid[x][y+1] == 0)):
-                    perimeter += 1
+                    perimeter += 2
                 if ((grid[x-1][y] == 0) or (grid[x+1][y] == 0)):
-                    perimeter += 1
-    return perimeter * 2
+                    perimeter += 2
+    return perimeter 
