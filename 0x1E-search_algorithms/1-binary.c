93d7940fb37627c8f25472eaf7acbb5dfc56a75d
Lucha3sr <2278@holbertonschool.com>
2021-04-20 18:15:12 +0000

    Search Algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,37 +7,50 @@
  *
  * Return: first index where value is located
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-size_t mid = size / 2;
-size_t i = 0;
+int index = 0;
 
+if (!array)
+return (-1);
+index = forUchecker(array, value, 0, size - 1, size);
+return (index);
+}
+
+/**
+ * forUchecker - searches for a value in an array of integers
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search
+ * @low: lowest value of the subarray
+ * @high: highest value of the subarray
+ *
+ * Return: first index where value is located
+ */
+int forUchecker(int *array, int value, int low, int high, size_t size)
+{
+int mid = low + (high - low) / 2;
+int i = 0;
+
+if (high >= low)
+{
 printf("Searching in array: ");
-for (i = 0; i < size - 1; i++)
+for (i = low; i <= high; i++)
+{
+if (i == high)
+printf("%d", array[i]);
+else
 printf("%d, ", array[i]);
+}
 printf("\n");
-if (array == NULL)
-return (-1);
-if (size <= 1)
-{
-if (array[0] == value)
-return (0);
-else
-return (-1);
-}
+/* If found at mid, then return it */
 if (array[mid] == value)
-{
 return (mid);
-}
+/* Search the left half */
 if (array[mid] > value)
-return (binary_search(array, mid, value));
-if (array[mid] < value)
-{
-if (mid % 2 != 0)
-return (binary_search(array, size - 1, value));
-else
-return (binary_search(array, size, value));
+return (forUchecker(array, value, low, mid - 1, size));
+/* Search the right half */
+return (forUchecker(array, value, mid + 1, high, size));
 }
 return (-1);
 }
