e081c26bae1d60b73ae3fa9c10f9a1874c38cbb7
Lucha3sr <2278@holbertonschool.com>
2021-04-20 16:08:15 +0000

    Search Algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,34 +10,34 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-  size_t mid = size / 2;
+size_t mid = size / 2;
+size_t i = 0;
 
-  printf("Searching in array: ");
-  for (size_t i = 0; i < size - 1; i++)
-    printf("%d, ", array[i]);
-  printf("\n");
-  if (array == NULL)
-    return (-1);
-  if (size <= 1)
-    {
-      if (array[0] == value)
-	return (0);
-      else
-	return (-1);
-    }
-  if (array[mid] == value)
-    {
-      return (mid);
-      
-    }
-  if (array[mid] > value)
-    return (binary_search(array, mid, value));
-  if (array[mid] < value)
-    {
-      if (mid % 2 != 0)
-	return (binary_search(array, size - 1, value));
-      else
-	return (binary_search(array, size, value));
-    }
-  return (-1);
+printf("Searching in array: ");
+for (i = 0; i < size - 1; i++)
+printf("%d, ", array[i]);
+printf("\n");
+if (array == NULL)
+return (-1);
+if (size <= 1)
+{
+if (array[0] == value)
+return (0);
+else
+return (-1);
+}
+if (array[mid] == value)
+{
+return (mid);
+}
+if (array[mid] > value)
+return (binary_search(array, mid, value));
+if (array[mid] < value)
+{
+if (mid % 2 != 0)
+return (binary_search(array, size - 1, value));
+else
+return (binary_search(array, size, value));
+}
+return (-1);
 }
